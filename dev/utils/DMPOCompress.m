d5abd1b4e09ae2832bdcf6866e3dd9feae39e449
Oliver Thomson Brown <otbrown91@gmail.com>
2016-02-17 14:17:13 +0000

    DMPOCompress.m: now works on the last site

--- dev/utils/DMPOCompress.m
+++ dev/utils/DMPOCompress.m
@@ -29,37 +29,55 @@ function [compDMPO] = DMPOCompress(dmpo, COMPRESS)
 	% allocate return
 	compDMPO = dmpo;
 
-	% THIS GIVES CORRECT SHAPE BUT DRASTICALLY ALTERS TRACE
 	for site = 1 : 1 : LENGTH - 1
 		[rowSz, colSz, ~, ~] = size(compDMPO{site});
+		% only need to modify tensors that are too large
+		if colSz > COMPRESS || rowSz > COMPRESS
+			M = reshape(compDMPO{site}, [rowSz, colSz, HILBY^2]);
+			M = permute(M, [1, 3, 2]);
+			M = reshape(M, [rowSz * HILBY^2, colSz]);
 
-		
+			[Q, R] = qr(M, 0);
 
-		M = reshape(compDMPO{site}, [rowSz, colSz, HILBY^2]);
+			colSz = size(Q, 2);
+
+			Q = reshape(Q, [rowSz, HILBY^2, colSz]);
+			Q = permute(Q, [1, 3, 2]);
+			Q = reshape(Q, [rowSz, colSz, HILBY, HILBY]);
+
+			% actual compression kicks in here
+			rowSz = min(rowSz, COMPRESS);
+			colSz = min(colSz, COMPRESS);
+			compDMPO{site} = Q(1 : rowSz, 1 : colSz, :, :);
+
+			% move R into the next site
+			rowSz = size(R, 1);
+			colSz = size(compDMPO{site + 1}, 2);
+			N = zeros(rowSz, colSz, HILBY, HILBY);
+			for bra = 1 : 1 : HILBY
+				for ket = 1 : 1 : HILBY
+					N(:, :, bra, ket) = R * compDMPO{site + 1}(:, :, bra, ket);
+				end
+			end
+			compDMPO{site + 1} = N;
+		end
+	end
+
+	% last site requires special handling,
+	% since we can't multiply R into the next site
+	if size(compDMPO{LENGTH}, 1) > COMPRESS
+		rowSz = size(compDMPO{LENGTH}, 1);
+		M = reshape(compDMPO{LENGTH}, [rowSz, 1, HILBY^2]);
 		M = permute(M, [1, 3, 2]);
-		M = reshape(M, [rowSz * HILBY^2, colSz]);
+		M = reshape(M, [rowSz * HILBY^2, 1]);
 
 		[Q, R] = qr(M, 0);
 
-		colSz = size(Q,2);
-
-		Q = reshape(Q, [rowSz, HILBY^2, colSz]);
+		Q = reshape(Q, [rowSz, HILBY^2, 1]);
 		Q = permute(Q, [1, 3, 2]);
-		Q = reshape(Q, [rowSz, colSz, HILBY, HILBY]);
+		Q = reshape(Q, [rowSz, 1, HILBY, HILBY]);
 
-		% actual compression kicks in here
-		rowSz = min(rowSz, COMPRESS);
-		colSz = min(colSz, COMPRESS);
-		compDMPO{site} = Q(1 : rowSz, 1 : colSz, :, :);
-
-		rowSz = size(R, 1);
-		colSz = size(compDMPO{site + 1}, 2);
-		N = zeros(rowSz, colSz, HILBY, HILBY);
-		for bra = 1 : 1 : HILBY
-			for ket = 1 : 1 : HILBY
-				N(:, :, bra, ket) = R * compDMPO{site + 1}(:, :, bra, ket);
-			end
-		end
-		compDMPO{site + 1} = N;
+		% compression occurs here
+		compDMPO{LENGTH} = Q(1 : COMPRESS, 1, :, :) * R;
 	end
 end
