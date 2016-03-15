e77c13c80c2d7545e5e0cd14b5b1e25533a412f0
Oliver Thomson Brown <otbrown91@gmail.com>
2016-03-15 11:51:31 +0000

    LCanTest.m: initial commit, usual tests + trace preservation, unitarity testing, and DM preservation

---
+++ testing/utilTests/LCanTest.m
@@ -0,0 +1,86 @@
+% LCanTest.m
+% Oliver Thomson Brown
+% 2016-03-15
+
+classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev')}) LCanTest < matlab.unittest.TestCase
+
+    properties
+        absTol = 1E-14;
+        HILBY = 2;
+        LENGTH = 7;
+        COMPRESS = 100;
+        SAMPLE_MAX = 100;
+        dmpo;
+        canDMPO;
+    end
+
+    methods (TestMethodSetup)
+        function MethodSetup(tc)
+            tc.dmpo = DMPO(tc.HILBY, tc.LENGTH, tc.COMPRESS);
+            tc.canDMPO = LCan(tc.dmpo, [1 : 1 : tc.LENGTH - 1]);
+        end
+    end
+
+    methods (Test)
+        function testClass(tc)
+            tc.fatalAssertClass(tc.canDMPO, 'cell');
+        end
+
+        function testSystemSize(tc)
+            tc.fatalAssertSize(tc.canDMPO, size(tc.dmpo));
+        end
+
+        function testTensorShape(tc)
+            % LCan should not alter dimensions, including virtual
+            for site = 1 : 1 : tc.LENGTH
+                tc.assertSize(tc.canDMPO{site}, size(tc.dmpo{site}));
+            end
+        end
+
+        function testTracePreservation(tc)
+            tr1 = DMPOTrace(tc.dmpo);
+            tr2 = DMPOTrace(tc.canDMPO);
+            epsilon = abs(abs(tr1) - abs(tr2));
+            tc.assertLessThan(epsilon, tc.absTol);
+        end
+
+        function testCan(tc)
+            % should be able to re-arrange each tensor back to the unitary from
+            % which it was formed -- A' * A = I
+            for site = 1 : 1 : tc.LENGTH - 1
+                [rowSz, colSz, ~, ~] = size(tc.canDMPO{site});
+                M = reshape(tc.canDMPO{site}, [rowSz, colSz, tc.HILBY^2]);
+                M = permute(M, [1, 3, 2]);
+                M = reshape(M, [rowSz * tc.HILBY^2, colSz]);
+
+                I = eye(colSz, colSz);
+                epsilon = (ctranspose(M) * M) - I;
+                tc.assertLessThan(epsilon, tc.absTol);
+            end
+        end
+
+        function testElements(tc)
+            % LCan should not alter the density matrix being represented
+            SPACE = tc.HILBY^tc.LENGTH;
+            sampleSz = min(floor(0.1 * SPACE^2), tc.SAMPLE_MAX);
+            for testNum = 1 : 1 : sampleSz
+                braState = randi([0, SPACE - 1]);
+                ketState = randi([0, SPACE - 1]);
+                braBits = FWBase(braState, tc.HILBY, tc.LENGTH) + 1;
+                ketBits = FWBase(ketState, tc.HILBY, tc.LENGTH) + 1;
+
+                coefft = 1;
+                canCoefft = 1;
+                for site = 1 : 1 : tc.LENGTH
+                    bra = braBits(site);
+                    ket = ketBits(site);
+                    coefft = coefft * tc.dmpo{site}(:, :, bra, ket);
+                    canCoefft = canCoefft * tc.canDMPO{site}(:, :, bra, ket);
+                end
+
+                epsilon = abs(abs(coefft) - abs(canCoefft));
+                tc.assertLessThan(epsilon, tc.absTol);
+            end
+        end
+    end
+end
