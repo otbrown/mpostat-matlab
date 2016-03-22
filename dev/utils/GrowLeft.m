590a46a1125ea6a5c8e5bdadc731aa0642ab63f1
Oliver Thomson Brown <otbrown91@gmail.com>
2016-03-22 16:38:11 +0000

    GrowLeft.m: added docstring

--- dev/utils/GrowLeft.m
+++ dev/utils/GrowLeft.m
@@ -1,6 +1,28 @@
 % GrowLeft.m
+% contracts a given dmpo site tensor into the left block -- a contraction from the
+% first site through to the site prior to this one.
+% NAIEVE IMPLEMENTATION -- OPTIMISE!
 % Oliver Thomson Brown
 % 2016-03-22
+%
+% updateBlock = GrowLeft(siteTensor, mpo, leftBlock, ROW_SIZE, COL_SIZE, HILBY, OP_ROW, OP_COL)
+%
+% RETURN
+% updateBlock   : complex double array, rank-3, contraction from the first site in %                 the system through to the specified site
+%
+% INPUTS
+% siteTensor    : complex double array, rank-4, the dmpo tensor for the specified
+%                 site -- to be included in the contraction from the first site in %                 the system
+% mpo           : complex double array, rank-6, mpo tensor for the operator which
+%                 is being evaluated in <rho|| O ||rho>
+% leftBlock     : complex double array, rank-3, the contraction from the first
+%                 site through to the site immediately before the specified site
+%                 -- if the specified site is the first, then leftBlock = 1
+% ROW_SIZE      : integer, the size of the first virtual dimension of siteTensor
+% COL_SIZE      : integer, the size of the second virtual dimension of siteTensor
+% HILBY         : integer, the size of the physical dimensions of siteTensor
+% OP_ROW        : integer, the size of the first virtual dimension of mpo
+% OP_COL        : integer, the size of the second virtual dimension of mpo
 
 function [updateBlock] = GrowLeft(siteTensor, mpo, leftBlock, ROW_SIZE, COL_SIZE, HILBY, OP_ROW, OP_COL)
 
