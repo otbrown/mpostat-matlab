9dd46451c44c61b26d5b5d80729446ac6f8f12ec
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-03-30 15:47:19 +0100

    GrowRight.m: added docstring

--- dev/utils/GrowRight.m
+++ dev/utils/GrowRight.m
@@ -1,6 +1,30 @@
 % GrowRight.m
+% contracts a given dmpo site tensor into the right block -- a
+% contraction from the last site in the system through to the site after
+% this one
 % Oliver Thomson Brown
 % 2016-03-22
+%
+% updateBlock = GrowRight(siteTensor, mpo, rightBlock, ROW_SIZE, COL_SIZE, HILBY, OP_ROW, OP_COL)
+%
+% RETURN
+% updateBlock       : rank-3 complex double array, contains the contraction from
+%                     the last site through to this one
+%
+% INPUTS
+% siteTensor        : rank-4 complex double array, the dmpo tensor for the
+%                     specified site
+% mpo               : rank-6 complex double array, mpo for the operator being
+%                     evaluated in the form <rho|| O ||rho>
+% rightBlock        : rank-3 complex double array, contains the contraction from
+%                     the last site through to the site immediately after this
+%                     one
+% ROW_SIZE          : integer, the size of the first virtual dimension of
+%                     siteTensor
+% COL_SIZE          : integer, the size of the second virtual dimension of
+%                     siteTensor
+% OP_ROW            : integer, the size of the first virtual dimension of mpo
+% OP_COL            : integer, the size of the second virtual dimension of mpo 
 
 function [updateBlock] = GrowRight(siteTensor, mpo, rightBlock, ROW_SIZE, COL_SIZE, HILBY, OP_ROW, OP_COL)
     updateBlock = zeros(ROW_SIZE, OP_ROW, ROW_SIZE);
@@ -38,5 +62,5 @@ function [updateBlock] = GrowRight(siteTensor, mpo, rightBlock, ROW_SIZE, COL_SI
                 updateBlock(conjCol, opRow, row) = BWFB;
             end
         end
-    end 
+    end
 end
