cf110f3940f14655faf3ffc7d7897b986bfce1ee
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-05-04 17:18:29 +0100

    EffOpTensorTest.m: initial commit, just a class test so far, but will compare elements in identity effective operator

---
+++ testing/utilTests/EffOpTensorTest.m
@@ -0,0 +1,53 @@
+% EffOpTensorTest.m
+% Oliver Thomson Brown
+% 2016-05-04
+% TRICKY TO TEST....
+
+classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev')}) EffOpTensorTest < matlab.unittest.TestCase
+
+    properties
+        absTol = 1E-15;
+        HILBY = 3;
+        LENGTH = 3;
+        COMPRESS = 0;
+        dmpo;
+        left;
+        impo;
+        right;
+        TEST_SITE;
+        effTen;
+    end
+
+    methods (TestMethodSetup)
+        function MethodSetup(tc)
+            tc.TEST_SITE = ceil(tc.LENGTH / 2);
+            tc.dmpo = ProdDMPO(tc.HILBY, tc.LENGTH, tc.COMPRESS, 0);
+            tc.impo = reshape(eye(tc.HILBY^2), [tc.HILBY, tc.HILBY, ...
+                        tc.HILBY, tc.HILBY]);
+            tc.left = cell(tc.LENGTH, 1);
+            tc.right = cell(tc.LENGTH, 1);
+            tc.left{1} = 1;
+            tc.right{tc.LENGTH} = 1;
+            for site = 1 : 1 : (tc.LENGTH - 1)
+                [ROW_SIZE, COL_SIZE, ~, ~] = size(tc.dmpo{site});
+                tc.left{site + 1} = GrowLeft(tc.dmpo{site}, tc.impo, ...
+                tc.left{site}, ROW_SIZE, COL_SIZE, tc.HILBY, 1, 1);
+            end
+            for site = tc.LENGTH : -1 : 2
+                [ROW_SIZE, COL_SIZE, ~, ~] = size(tc.dmpo{site});
+                tc.right{site - 1} = GrowRight(tc.dmpo{site}, tc.impo, ...
+                tc.right{site}, ROW_SIZE, COL_SIZE, tc.HILBY, 1, 1);
+            end
+
+            [ROW_SIZE, COL_SIZE, ~, ~] = size(tc.dmpo{tc.TEST_SITE});
+            tc.effTen = EffOpTensor(tc.left{tc.TEST_SITE}, tc.impo, ...
+            tc.right{tc.TEST_SITE}, ROW_SIZE, COL_SIZE, tc.HILBY, 1, 1);
+        end
+    end
+
+    methods (Test)
+        function testClass(tc)
+            tc.fatalAssertClass(tc.effTen, 'double');
+        end
+    end
+end
