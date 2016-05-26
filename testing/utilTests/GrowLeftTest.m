508586b6f6b250b5b329adc25e216e5fad7cbe82
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-05-26 10:28:56 +0100

    GrowLeftTest.m: added Liouvillian expectation test

--- testing/utilTests/GrowLeftTest.m
+++ testing/utilTests/GrowLeftTest.m
@@ -10,10 +10,12 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev')}
         HILBY;
         LENGTH;
         dmpo;
-        leftBlock;
+        rhoVec;
+        iLeft;
+        lLeft;
         impo;
-        OP_ROW = 1;
-        OP_COL = 1;
+        lmpo;
+        L;
     end
 
     properties (MethodSetupParameter)
@@ -25,16 +27,116 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev')}
         function MethodSetup(tc, testHILBY, testLENGTH)
             tc.HILBY = testHILBY;
             tc.LENGTH = testLENGTH;
-            tc.dmpo = DMPO(tc.HILBY, tc.LENGTH, tc.COMPRESS);
+            tc.dmpo = SuperDMPO(tc.HILBY, tc.LENGTH, tc.COMPRESS);
             tc.dmpo = LCan(tc.dmpo, [1 : 1 : (tc.LENGTH - 1)]);
-            tc.leftBlock = cell(tc.LENGTH, 1);
-            tc.leftBlock{1} = 1;
+            tc.rhoVec = ones(tc.HILBY^(2*tc.LENGTH), 1) / (tc.HILBY^tc.LENGTH);
+            tc.iLeft = cell(tc.LENGTH, 1);
+            tc.lLeft = cell(tc.LENGTH, 1);
+            tc.iLeft{1} = 1;
+            tc.lLeft{1} = 1;
             tc.impo = reshape(eye(tc.HILBY^2), [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            tc.lmpo = cell(tc.LENGTH, 1);
+
+            % build liouvillian mpo
+            mpo = zeros(tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY, 6, 6);
+
+            U = 1;
+            J = 0.5;
+            gamma = 0.1;
+
+            I = eye(tc.HILBY);
+            a = zeros(tc.HILBY);
+            for i = 1 : 1 : (tc.HILBY - 1)
+                a(i, i + 1) = sqrt(i);
+            end
+
+            ident = kron(I, I);
+            Hloc = zeros(tc.HILBY);
+            for i = 2 : 1 : tc.HILBY
+                Hloc(i, i) = i * U;
+            end
+            dissipator = 0.5 * gamma * (kron(2 * conj(a), a) ...
+                         - kron(I, ctranspose(a) * a) ...
+                         - kron(transpose(a) * conj(a), I));
+            mpoLoc = kron(I, -1i*Hloc) + kron(1i*Hloc, I) + dissipator;
+
+            mpo(:, :, :, :, 6, 1) = reshape(mpoLoc, ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 1, 1) = reshape(ident, ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 6, 6) = reshape(ident, ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 2, 1) = reshape(kron(I, a), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 3, 1) = reshape(kron(a, I), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 4, 1) = reshape(kron(I, ctranspose(a)), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 5, 1) = reshape(kron(ctranspose(a), I), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 6, 2) = reshape(kron(I, 1i*J*ctranspose(a)), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 6, 3) = reshape(kron(-1i*J*ctranspose(a), I), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 6, 4) = reshape(kron(I, 1i*J*a), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+            mpo(:, :, :, :, 6, 5) = reshape(kron(-1i*J*a, I), ...
+                                     [tc.HILBY, tc.HILBY, tc.HILBY, tc.HILBY]);
+
+            tc.lmpo{1} = mpo(:, :, :, :, 6, :);
+            tc.lmpo{tc.LENGTH} = mpo(:, :, :, :, :, 1);
+            for site = 2 : 1 : (tc.LENGTH - 1)
+                tc.lmpo{site} = mpo;
+            end
 
             % populate leftBlock
-            for site = 2 : 1 : tc.LENGTH
-                [rowSz, colSz, ~, ~] = size(tc.dmpo{site - 1});
-                tc.leftBlock{site} = GrowLeft(tc.dmpo{site - 1}, tc.impo, tc.leftBlock{site - 1}, rowSz, colSz, tc.HILBY, tc.OP_ROW, tc.OP_COL);
+            for site = 1 : 1 : (tc.LENGTH - 1)
+                [rowSz, colSz, ~, ~] = size(tc.dmpo{site});
+                [~, ~, ~, ~, OP_ROW, OP_COL] = size(tc.lmpo{site});
+                tc.iLeft{site + 1} = GrowLeft(tc.dmpo{site}, tc.impo, tc.iLeft{site}, rowSz, colSz, tc.HILBY, 1, 1);
+                tc.lLeft{site + 1} = GrowLeft(tc.dmpo{site}, tc.lmpo{site}, tc.lLeft{site}, rowSz, colSz, tc.HILBY, OP_ROW, OP_COL);
+            end
+
+            % build Liouvillian
+            IH = eye(tc.HILBY^tc.LENGTH);
+            a_n = zeros(tc.HILBY^tc.LENGTH, tc.HILBY^tc.LENGTH, tc.LENGTH);
+            H_n = zeros(tc.HILBY^tc.LENGTH, tc.HILBY^tc.LENGTH, tc.LENGTH);
+            for target = 1 : 1 : tc.LENGTH
+                op = 1;
+                ham = 1;
+                for site = 1 : 1 : (target - 1)
+                    op = kron(op, I);
+                    ham = kron(ham, I);
+                end
+                op = kron(op, a);
+                ham = kron(ham, Hloc);
+                for site = (target + 1) : 1 : tc.LENGTH
+                    op = kron(op, I);
+                    ham = kron(ham, I);
+                end
+                a_n(:,:,target) = op;
+                H_n(:,:,target) = ham;
+            end
+
+            Hint = zeros(tc.HILBY^tc.LENGTH);
+            for site = 1 : 1 : (tc.LENGTH - 1)
+                Hint = Hint + a_n(:,:,site) * ctranspose(a_n(:,:,site + 1)) ...
+                            + ctranspose(a_n(:,:,site)) * a_n(:,:,site+1);
+            end
+            Hint = - J * Hint;
+
+            H = zeros(tc.HILBY^tc.LENGTH);
+            for site = 1 : 1 : tc.LENGTH
+                H = H + H_n(:, :, site);
+            end
+            H = H + Hint;
+
+            tc.L = -1i * (kron(IH, H) - kron(H, IH));
+            for site = 1 : 1 : tc.LENGTH
+                tc.L = tc.L + 0.5 * gamma ...
+                * (kron(2*conj(a_n(:,:,site)), a_n(:,:,site)) ...
+                - kron(IH, ctranspose(a_n(:,:,site))*a_n(:,:,site)) ...
+                - kron(transpose(a_n(:,:,site))*a_n(:,:,site), IH));
             end
         end
     end
@@ -43,33 +145,25 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev')}
         function vecNormTest(tc)
             % calculate vector norm by using LeftGrow past the end of the system
             [rowSz, colSz, ~, ~] = size(tc.dmpo{tc.LENGTH});
-            testVecNorm = GrowLeft(tc.dmpo{tc.LENGTH}, tc.impo, tc.leftBlock{tc.LENGTH}, rowSz, colSz, tc.HILBY, tc.OP_ROW, tc.OP_COL);
+            testVecNorm = GrowLeft(tc.dmpo{tc.LENGTH}, tc.impo, tc.iLeft{tc.LENGTH}, rowSz, colSz, tc.HILBY, 1, 1);
 
             % calculate the vector norm the old fashioned way...
-            % RETURN ALLOCATION
-            SPACE = tc.HILBY^tc.LENGTH;
-            rhoVec = zeros(SPACE^2, 1);
-
-            % REBUILD VECTORISED DENSITY MATRIX
-            for ketState = 0 : 1 : (SPACE - 1)
-                for braState = 0 : 1 : (SPACE - 1)
-                    stateDex = ketState * SPACE + braState + 1;
-                    braBits = FWBase(braState, tc.HILBY, tc.LENGTH) + 1;
-                    ketBits = FWBase(ketState, tc.HILBY, tc.LENGTH) + 1;
-
-                    coefft = 1;
-                    for site = 1 : 1 : tc.LENGTH
-                        bra = braBits(site);
-                        ket = ketBits(site);
-                        coefft = coefft * tc.dmpo{site}(:, :, bra, ket);
-                    end
-
-                    rhoVec(stateDex) = coefft;
-                end
-            end
-            vecNorm = ctranspose(rhoVec) * rhoVec;
+            vecNorm = ctranspose(tc.rhoVec) * tc.rhoVec;
 
             epsilon = abs(testVecNorm - vecNorm);
+            tc.assertLessThan(epsilon, tc.absTol);
+        end
+
+        function liouvExpTest(tc)
+            % calculate Liouvillian expectation by growin g past the last site
+            [rowSz, colSz, ~, ~] = size(tc.dmpo{tc.LENGTH});
+            testLExp = GrowLeft(tc.dmpo{tc.LENGTH}, tc.lmpo{tc.LENGTH}, tc.lLeft{tc.LENGTH}, rowSz, colSz, tc.HILBY, 6, 1);
+
+            % calculate the Liouvillian expectation the old-fashioned way
+            lExp = ctranspose(tc.rhoVec) * tc.L * tc.rhoVec;
+            fprintf(1, 'lExp: %g\n', lExp);
+
+            epsilon = abs(testLExp - lExp);
             tc.assertLessThan(epsilon, tc.absTol);
         end
     end
