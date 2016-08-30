a964cfe96ef0d93336035bd89dc9d4690601da3e
Oliver Thomson Brown <otbrown91@gmail.com>
2016-08-30 12:21:52 +0100

    runTests.m: initial commit, ready for deployment to workstation to run full test-suite (hopefully) -- can't test on my laptop due to RAM, issue #1

---
+++ runTests.m
@@ -0,0 +1,17 @@
+% runTests.m
+% platform independent script to run test-suite on mpostat-matlab and save
+% results, eases remote deployment by creating a single call for the suite
+% and separating results
+% Oliver Thomson Brown
+% 2016-08-30
+
+% set up result area
+resultDir = 'testing/results';
+mkdir(resultDir);
+dateStamp = datestr(datetime('today'), 30);
+savePath = sprintf('%s/%s_TEST.mat', resultDir, dateStamp);
+
+%result = runtests('testing', 'IncludeSubFolders', true);
+result = runtests('testing/utilTests');
+
+save(savePath);
