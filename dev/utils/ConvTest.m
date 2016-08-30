37fd2ed74f02d543e43161b7803d8c00938e383e
Oliver Thomson Brown <otbrown91@gmail.com>
2016-08-30 15:30:43 +0100

    ConvTest.m: initial commit, tests convergence of lowest Liouvillian eigenvalue to zero, test passes if eig is below THRESHOLD

---
+++ dev/utils/ConvTest.m
@@ -0,0 +1,33 @@
+
+% ConvTest.m
+% function which checks if the last sampleSize eigenvalues have been below
+% threshold
+% Oliver Thomson Brown
+% 2016-08-30
+%
+% [RETURN]
+% convFlag	: test result, convFlag == 1.0 if successful and convFlag == 0.0 if
+%             test is failed
+%
+% [INPUTS]
+% eigTracker	: complex array, contains the eigenvalue of the effective
+%                 Liouvillian solved for each site update
+% updCount      : double, the number of site updates performed so far
+% sampleSize	: double, the number of results to check for convergence
+% threshold	    : double, convergence threshold
+
+function [ convFlag ] = ConvTest(eigTracker, updCount, sampleSize, threshold)
+	convFlag = 0;
+
+	if sampleSize >= updCount
+        % total number of updates is still less than sampleSize
+        % do nothing
+	else
+		sample = eigTracker(updCount - sampleSize : updCount) - ...
+                 eigTracker(updCount);
+		worstCase = max( abs(sample) );
+		if worstCase < threshold
+			convFlag = 1;
+		end
+	end
+end
