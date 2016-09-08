6eb66ba39a027539903ab9a3dbfb7b43fc24e59d
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-08 17:58:27 +0100

    validate.m: initial commit, runs unit tests, followed by validation tests

---
+++ validate.m
@@ -0,0 +1,23 @@
+% validate.m
+% runs both unit testing and the validation tests
+% Oliver Thomson Brown
+% 2016-09-08
+
+% set up result area
+resultDir = 'testing/results';
+if ~exist(resultDir, 'dir')
+    mkdir(resultDir);
+end
+
+utilResult = runtests('testing/utilTests');
+utilResult = runtests('testing/utilTests');
+utilResult = runtests('testing/utilTests');
+valResult = runtests('testing/validationTests');
+
+failCount = sum([utilResult.Failed]) + sum([coreResult.Failed]) ...
+            + sum([topResult.Failed]) + sum([valResult.Failed]);
+if failCount
+    dateStamp = datestr(datetime('now'), 30);
+    savePath = sprintf('%s/%s_VALIDATE.mat', resultDir, dateStamp);
+    save(savePath);
+end
