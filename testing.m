eebe5c4f66032e435257ed336c063cc79b0117c4
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-08 17:57:52 +0100

    runTests.m: renamed testing.m, will only try and create results folder if it doesn't already exist, only creates a save file in the event of a failure, does not run validation tests

---
+++ testing.m
@@ -0,0 +1,24 @@
+% testing.m
+% platform independent script to run test-suite on mpostat-matlab and save
+% results, eases remote deployment by creating a single call for the suite
+% and separating results
+% Oliver Thomson Brown
+% 2016-08-30
+
+% set up result area
+resultDir = 'testing/results';
+if ~exist(resultDir, 'dir')
+    mkdir(resultDir);
+end
+
+utilResult = runtests('testing/utilTests');
+utilResult = runtests('testing/utilTests');
+utilResult = runtests('testing/utilTests');
+
+failCount = sum([utilResult.Failed]) + sum([coreResult.Failed]) ...
+            + sum([topResult.Failed]);
+if failCount
+    dateStamp = datestr(datetime('now'), 30);
+    savePath = sprintf('%s/%s_TESTING.mat', resultDir, dateStamp);
+    save(savePath);
+end
