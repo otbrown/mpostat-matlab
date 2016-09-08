60c5fdf47fbc4504487e0ef693f184d5161de30b
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-08 18:05:00 +0100

    ConvTestTest.m: calls to ConvTest modified to include convergence, additional test added to check sign changes about zero, issue #1

--- testing/utilTests/ConvTestTest.m
+++ testing/utilTests/ConvTestTest.m
@@ -14,10 +14,12 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
 
     methods (Test)
         function testTooFewUpdates(tc)
-            convFlag = ConvTest(tc.eigTracker, tc.sampleSize - 1, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(tc.eigTracker, tc.sampleSize - 1, tc.sampleSize, tc.THRESHOLD);
+            tc.assertTrue(isnan(convergence));
             tc.assertEqual(convFlag, tc.FALSE);
 
-            convFlag = ConvTest(tc.eigTracker, tc.sampleSize, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(tc.eigTracker, tc.sampleSize, tc.sampleSize, tc.THRESHOLD);
+            tc.assertTrue(isnan(convergence));
             tc.assertEqual(convFlag, tc.FALSE);
         end
 
@@ -25,12 +27,14 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
             eigTracker = tc.eigTracker;
             updCount = 2 * tc.sampleSize;
             eigTracker(1 : updCount) = rand(updCount, 1);
-            convFlag = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            tc.assertGreaterThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.FALSE);
 
             eigTracker(1 : updCount) = ones(updCount, 1);
             eigTracker(updCount) = 1 + tc.THRESHOLD;
-            convFlag = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            tc.assertGreaterThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.FALSE);
         end
 
@@ -38,11 +42,19 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
             eigTracker = tc.eigTracker;
             updCount = 2 * tc.sampleSize;
             eigTracker(1 : updCount) = ones(updCount, 1);
-            convFlag = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            tc.assertLessThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.TRUE);
 
             eigTracker(1 : updCount) = ones(updCount, 1) + tc.THRESHOLD * rand(updCount, 1);
-            convFlag = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            tc.assertLessThan(convergence, tc.THRESHOLD);
+            tc.assertEqual(convFlag, tc.TRUE);
+
+            eigTracker(1 : updCount) = zeros(updCount, 1) + (tc.THRESHOLD/10) * rand(updCount, 1);
+            eigTracker(1 : 2 : updCount) = -1 * eigTracker(1 : 2 : updCount);
+            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            tc.assertLessThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.TRUE);
         end
     end
