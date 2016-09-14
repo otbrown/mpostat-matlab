eb355b0a0f027b5075371f1877c8bae98841ad83
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-14 11:12:34 +0100

    EffL.m: #2, replaced for loops with permute and reshape, 0.209s

--- dev/core/EffL.m
+++ dev/core/EffL.m
@@ -30,33 +30,7 @@ function [effectiveLiouv] = EffL(TARGET, dmpo, mpo, left, right)
     effTensor = EffOpTensor(lBlock, siteMPO, rBlock, ROW_SIZE, COL_SIZE, HILBY);
 
     % reshape and return
-    %effTensor = permute(effTensor, [3, 4, 8, 7, 1, 2, 6, 5]);
-    %effectiveLiouv = reshape(effTensor, [ROW_SIZE*COL_SIZE*HILBY*HILBY, ...
-    %                                     ROW_SIZE*COL_SIZE*HILBY*HILBY]);
-
-
-    effectiveLiouv = zeros(ROW_SIZE*COL_SIZE*HILBY*HILBY);
-    for conjBra = 0 : 1 : (HILBY - 1)
-        for conjKet = 0 : 1 : (HILBY - 1)
-            for conjCol = 0 : 1 : (ROW_SIZE - 1)
-                for conjRow = 1 : 1 : COL_SIZE
-                    jRow = conjKet * HILBY * ROW_SIZE * COL_SIZE ...
-                           + conjBra * ROW_SIZE * COL_SIZE ...
-                           + conjCol * COL_SIZE + conjRow;
-                    for bra = 0 : 1 : (HILBY - 1)
-                        for ket = 0 : 1 : (HILBY - 1)
-                            for row = 0 : 1 : (ROW_SIZE - 1)
-                                for col = 1 : 1 : COL_SIZE
-                                    jCol = ket * HILBY * ROW_SIZE * COL_SIZE + bra * ROW_SIZE * COL_SIZE + row * COL_SIZE + col;
-                                    effectiveLiouv(jRow, jCol) = ...
-                                    effTensor(row+1, col, conjRow, conjCol+1, bra+1, ket+1, conjBra+1, conjKet+1);
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-
+    effTensor = permute(effTensor, [3, 4, 7, 8, 2, 1, 5, 6]);
+    effectiveLiouv = reshape(effTensor, [ROW_SIZE*COL_SIZE*HILBY*HILBY, ...
+                                         ROW_SIZE*COL_SIZE*HILBY*HILBY]);
 end
