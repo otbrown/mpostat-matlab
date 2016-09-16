84e4e655cdcef395a84731d221f9a49414822fb1
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-16 12:28:51 +0100

    ConvTestTest.m: #8, updated to test new convergence test

--- testing/utilTests/ConvTestTest.m
+++ testing/utilTests/ConvTestTest.m
@@ -6,54 +6,54 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
 
     properties
         THRESHOLD = 1E-7;
-        eigTracker = NaN(100, 1);
-        sampleSize = 25;
+        LENGTH = 30;
         FALSE = 0;
         TRUE = 1;
     end
 
     methods (Test)
         function testTooFewUpdates(tc)
-            [convFlag, convergence] = ConvTest(tc.eigTracker, tc.sampleSize - 1, tc.sampleSize, tc.THRESHOLD);
+            eigTracker = NaN(tc.LENGTH, 1);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertTrue(isnan(convergence));
             tc.assertEqual(convFlag, tc.FALSE);
 
-            [convFlag, convergence] = ConvTest(tc.eigTracker, tc.sampleSize, tc.sampleSize, tc.THRESHOLD);
+            eigTracker(1 : tc.LENGTH - 1) = 0;
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertTrue(isnan(convergence));
             tc.assertEqual(convFlag, tc.FALSE);
         end
 
         function testNotConverged(tc)
-            eigTracker = tc.eigTracker;
-            updCount = 2 * tc.sampleSize;
-            eigTracker(1 : updCount) = rand(updCount, 1);
-            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            eigTracker = rand(tc.LENGTH, 1);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertGreaterThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.FALSE);
 
-            eigTracker(1 : updCount) = ones(updCount, 1);
-            eigTracker(updCount) = 1 + tc.THRESHOLD;
-            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            eigTracker = tc.LENGTH * tc.THRESHOLD  * eigTracker;
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
+            tc.assertGreaterThan(convergence, tc.THRESHOLD);
+            tc.assertEqual(convFlag, tc.FALSE);
+
+            eigTracker(1 : 2 : tc.LENGTH) = -1 * eigTracker(1 : 2 : tc.LENGTH);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertGreaterThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.FALSE);
         end
 
         function testConverged(tc)
-            eigTracker = tc.eigTracker;
-            updCount = 2 * tc.sampleSize;
-            eigTracker(1 : updCount) = ones(updCount, 1);
-            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            eigTracker = ones(tc.LENGTH, 1);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertLessThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.TRUE);
 
-            eigTracker(1 : updCount) = ones(updCount, 1) + tc.THRESHOLD * rand(updCount, 1);
-            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            eigTracker = tc.THRESHOLD * rand(tc.LENGTH, 1);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertLessThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.TRUE);
 
-            eigTracker(1 : updCount) = zeros(updCount, 1) + (tc.THRESHOLD/10) * rand(updCount, 1);
-            eigTracker(1 : 2 : updCount) = -1 * eigTracker(1 : 2 : updCount);
-            [convFlag, convergence] = ConvTest(eigTracker, updCount, tc.sampleSize, tc.THRESHOLD);
+            eigTracker(1 : 2 : tc.LENGTH) = -0.5 * eigTracker(1 : 2 : tc.LENGTH);
+            [convFlag, convergence] = ConvTest(eigTracker, tc.THRESHOLD);
             tc.assertLessThan(convergence, tc.THRESHOLD);
             tc.assertEqual(convFlag, tc.TRUE);
         end
