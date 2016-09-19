b81834a370b917a7564f0c59b6d6123d60ba2afc
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-09-19 16:09:33 +0100

    LCan.m: updated docstring, removed commented out code, and rationalised some multiplications

--- dev/utils/LCan.m
+++ dev/utils/LCan.m
@@ -12,11 +12,12 @@
 %
 % INPUTS
 % dmpo      : cell array, a density matrix product operator
-% route     : integer array, the sites which should be brought into left-canonical
-%             form, in increasing order -- note that the last site cannot be the
-%             last site in the system, as the routine multiplies into the next
-%             site along. If a single integer is supplied, only that site is
-%             into canonical form, but the next site along is still affected
+% route     : integer array, the sites which should be brought into
+%             left-canonical form, in increasing order -- note that the last
+%             site cannot be the last site in the system, as the routine
+%             multiplies into the next site along. If a single integer is
+%             supplied, only that site is into canonical form, but the next
+%             site along is still affected
 
 function [ldmpo] = LCan(dmpo, route)
     % gather constants
@@ -33,56 +34,37 @@ function [ldmpo] = LCan(dmpo, route)
     % allocate return
     ldmpo = dmpo;
 
+    [rowSz, colSz, ~, ~] = size(ldmpo{route(1)});
+
     for site = route
         % manipulate site tensor into a matrix
-        [rowSz, colSz, ~, ~] = size(ldmpo{site});
         M = reshape(ldmpo{site}, [rowSz, colSz, HILBY^2]);
         M = permute(M, [1, 3, 2]);
         M = reshape(M, [rowSz * HILBY^2, colSz]);
 
-        % QR decomposition
-        %[Q, R] = qr(M,0);
         % SVD Decomposition
         [U, S, V] = svd(M, 'econ');
-        V = ctranspose(V);
-        dim = size(U, 2);
+        uc = size(U, 2);
 
-        %colSz = size(Q, 2);
-        %colSz = size(US, 2);
-
-        % manipulate Q into rank-4 tensor and embed in site
-        %Q = reshape(Q, [rowSz, HILBY^2, colSz]);
-        %Q = permute(Q, [1, 3, 2]);
-        %ldmpo{site} = reshape(Q, [rowSz, colSz, HILBY, HILBY]);
-        % reshape M to be the new site tensor
-        %M = reshape(US, [rowSz, HILBY^2, colSz]);
-        %M = permute(US, [1, 3, 2]);
-        %ldmpo{site} = reshape(US, [rowSz, colSz, HILBY, HILBY]);
+        % manipulate U into rank-4 tensor and embed in site
         U2 = zeros(rowSz * HILBY^2, colSz);
-        U2(:, 1 : dim) = U;
+        U2(:, 1 : uc) = U;
         U2 = reshape(U2, [rowSz, HILBY^2, colSz]);
         U2 = permute(U2, [1, 3, 2]);
         ldmpo{site} = reshape(U2, [rowSz, colSz, HILBY, HILBY]);
 
         % multiply R into the next site along
-        %rowSz = size(R, 1);
-        %rowSz = size(V, 1);
-        %colSz = size(ldmpo{site+1}, 2);
-        %N = zeros(rowSz, colSz, HILBY, HILBY);
         rowSz = colSz;
         colSz = size(ldmpo{site + 1}, 2);
 
-        S2 = zeros(rowSz, dim);
-        S2(1 : dim, :) = S;
+        SV = zeros(rowSz);
+        SV(1 : uc, :) = S * ctranspose(V);
 
         for bra = 1 : 1 : HILBY
             for ket = 1 : 1 : HILBY
-                %N(:, :, bra, ket) = R * ldmpo{site + 1}(:, :, bra, ket);
-                %N(:, :, bra, ket) = V * ldmpo{site + 1}(:, :, bra, ket);
                 ldmpo{site + 1}(:, :, bra, ket) = ...
-                S2 * V * ldmpo{site + 1}(:, :, bra, ket);
+                SV * ldmpo{site + 1}(:, :, bra, ket);
             end
         end
-        %ldmpo{site + 1} = N;
     end
 end
