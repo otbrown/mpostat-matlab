3491ef009202f3e1e020fef2f0f7d7f2b792d616
Oliver Thomson Brown <otbrown91@gmail.com>
2017-02-08 13:28:47 +0000

    PhasedSearch.m: first finished version, #17

--- dev/top/PhasedSearch.m
+++ dev/top/PhasedSearch.m
@@ -1,6 +1,7 @@
 % PhasedSearch.m
 % top level function which carries out repeated stationary state searches,
-% gradually lowering the accuracy threshold, and increasing the matrix dimension
+% gradually lowering the accuracy threshold, and increasing the matrix
+% dimension
 % Oliver Thomson Brown
 % 2017-02-07
 %
@@ -8,21 +9,76 @@
 %                                         MAX_COMPRESS, variant)
 %
 % RETURN
-% dmpoStat:   cell, density matrix product operator representing the stationary
-%             state (hopefully)
-% phaseEigs:  (complex) double, contains the final eigenvalue of each phase of
-%             the calculation
+% dmpoStat:     cell, density matrix product operator representing the
+%               stationary state (hopefully)
+% phaseEigs:    (complex) double, contains the final eigenvalue of each
+%               phase ofthe calculation
 %
 % INPUT
-% HILBY:              integer, size of the local state space
-% LENGTH:             integer, the number of sites in the system
-% mpo:                cell, Liouvillian for the system in matrix product
-%                     operator form
-% ULTIMATE_THRESHOLD: double, how close must L*rho be to zero for the
-%                     calculation to be deemed successful, the calculation will
-%                     end once this is reached
-% MAX_COMPRESS:       integer, the maximum allowed matrix dimension of the
-%                     density matrix product operator
-% variant:            string, specifies whether to solve the non-Hermitian
-%                     Liouvillian, or the Hermitian product, 'direct' or
-%                     'hermitian'
+% HILBY:                integer, size of the local state space
+% LENGTH:               integer, the number of sites in the system
+% mpo:                  cell, Liouvillian for the system in matrix product
+%                       operator form
+% ULTIMATE_THRESHOLD:   double, how close must L*rho be to zero for the
+%                       calculation to be deemed successful, the
+%                       calculation will end once this is reached
+% MAX_COMPRESS:         integer, the maximum allowed matrix dimension of
+%                       the density matrix product operator
+% VARIANT:              string, specifies whether to solve the
+%                       non-Hermitian Liouvillian, or the Hermitian
+%                       product, 'direct' or 'hermitian'
+
+function [dmpoStat, phaseEigs] = PhasedSearch(HILBY, LENGTH, mpo, ULTIMATE_THRESHOLD, MAX_COMPRESS, VARIANT)
+    % set up
+    threshold = 0.01;
+    compress = HILBY^2;
+    phaseCount = 0;
+    phaseTrack = [];
+    phaseEig = Inf;
+    ARPACK_msgID = 'MATLAB:eigs:ARPACKroutineErrorMinus14';
+
+    dmpoStat = MixDMPO(HILBY, LENGTH, compress)
+
+    while phaseEig > ULTIMATE THRESHOLD
+        fprintf('PHASE %g:\n[ THRESHOLD: %g, COMPRESS: %g]\n', ...
+                phaseCount, threshold, compress);
+
+        dmpoStat = DMPOResize(dmpoStat, compress);
+
+        try
+            % solve using Stationary
+            [dmpoStat, eigTrack] = Stationary(dmpo, mpo, ...
+                                                threshold, VARIANT);
+            phaseEig = abs(eigTrack(end));
+        catch ME
+            if strcmpi(ME.identifier, ARPACK_msgID);
+                % DO NOTHING
+            else
+                throw(ME);
+            end
+        end
+
+        % Technically this is dangerous as phaseTrack could grow to
+        % infinite length. Best hope not, eh.
+        phaseTrack(end + 1) = phaseEig;
+
+        if phaseEig < threshold
+            if phaseEig < ULTIMATE_THRESHOLD
+                fprintf('PHASE SUCCESS. ULTIMATE THRESHOLD REACHED.\n');
+                fprintf('ENDING CALCULATION\n');
+            else
+                fprintf('PHASE SUCCESS. LOWERING THRESHOLD.\n\n');
+                threshold = max(ULTIMATE_THRESHOLD, threshold / 10);
+            end
+        else
+            if compress == MAX_COMPRESS
+                fprintf('PHASE FAILED. MATRIX DIMENSION LIMIT REACHED.\n');
+                fprintf('ENDING CALCULATION.\n');
+                break;
+            else
+                fprintf('PHASE FAILED. ENLARGING MATRIX DIMENSION.\n\n');
+                compress = min(MAX_COMPRESS, compress + HILBY^2);
+            end
+        end
+    end
+end
