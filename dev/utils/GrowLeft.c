6d645ca1e562a3a04e5c987668daebe55f057812
Oliver Thomson Brown <otbrown91@gmail.com>
2017-03-08 16:32:04 +0000

    GrowLeft.c: first working version -- segfault was caused by missing indexes when final index was 1, and by mxGetPi returning NULL on real arrays (duh..), #19

--- dev/utils/GrowLeft.c
+++ dev/utils/GrowLeft.c
@@ -15,147 +15,87 @@
 typedef struct array_Z3D
 {
   mwSize dims[3];
-  complex double ***arr;
+  complex double *arr;
 } az3_t;
 
 typedef struct array_Z4D
 {
   mwSize dims[4];
-  complex double ****arr;
+  complex double *arr;
 } az4_t;
 
 typedef struct array_Z6D
 {
   mwSize dims[6];
-  complex double ******arr;
+  complex double *arr;
 } az6_t;
 
-complex double ***init_Z3arr(const mwSize dims[3])
+az3_t *init_Z3arr(const mwSize dims[3])
 {
-  mwIndex dim0, dim1, dim2;
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2];
+  az3_t *A = mxMalloc(sizeof(az3_t));
+  mwIndex dim;
 
-  complex double ***arr = mxMalloc(sizeof(complex double **) * dims[0]);
-  for(dim0 = 0; dim0 < dims[0]; ++dim0) {
-    arr[dim0] = mxMalloc(sizeof(complex double *) * dims[1]);
-    for(dim1 = 0; dim1 < dims[1]; ++dim1) {
-      arr[dim0][dim1] = mxMalloc(sizeof(complex double) * dims[2]);
-    }
+  for(dim = 0; dim < 3; ++dim) {
+    (*A).dims[dim] = dims[dim];
   }
 
-  return arr;
+  (*A).arr = mxMalloc(sizeof(complex double) * NUMEL);
+
+  return A;
 }
 
-complex double ****init_Z4arr(const mwSize dims[4])
+az4_t *init_Z4arr(const mwSize dims[4])
 {
-  mwIndex dim0, dim1, dim2;
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2] * dims[3];
+  az4_t *A = mxMalloc(sizeof(az4_t));
+  mwIndex dim;
 
-  complex double ****arr = mxMalloc(sizeof(complex double ***) * dims[0]);
-  for(dim0 = 0; dim0 < dims[0]; ++dim0) {
-    arr[dim0] = mxMalloc(sizeof(complex double **) * dims[1]);
-    for(dim1 = 0; dim1 < dims[1]; ++dim1) {
-      arr[dim0][dim1] = mxMalloc(sizeof(complex double *) * dims[2]);
-      for(dim2 = 0; dim2 < dims[2]; ++dim2) {
-        arr[dim0][dim1][dim2] = mxMalloc(sizeof(complex double) * dims[3]);
-      }
-    }
+  for(dim = 0; dim < 4; ++dim) {
+    (*A).dims[dim] = dims[dim];
   }
-  return arr;
+
+  (*A).arr = mxMalloc(sizeof(complex double) * NUMEL);
+
+  return A;
 }
 
-complex double ******init_Z6arr(const mwSize dims[6])
+az6_t *init_Z6arr(const mwSize dims[6])
 {
-  mwIndex dim0, dim1, dim2, dim3, dim4;
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2]
+                        * dims[3] * dims[4] * dims[5];
+  az6_t *A = mxMalloc(sizeof(az6_t));
+  mwIndex dim;
 
-  complex double ******arr = mxMalloc(sizeof(complex double *****) * dims[0]);
-  for(dim0 = 0; dim0 < dims[0]; ++dim0) {
-    arr[dim0] = mxMalloc(sizeof(complex double ****) * dims[1]);
-    for(dim1 = 0; dim1 < dims[1]; ++dim1) {
-      arr[dim0][dim1] = mxMalloc(sizeof(complex double ***) * dims[2]);
-      for(dim2 = 0; dim2 < dims[2]; ++dim2) {
-        arr[dim0][dim1][dim2] = mxMalloc(sizeof(complex double **) * dims[3]);
-        for(dim3 = 0; dim3 < dims[3]; ++dim3) {
-          arr[dim0][dim1][dim2][dim3]
-                                = mxMalloc(sizeof(complex double *) * dims[4]);
-          for(dim4 = 0; dim4 < dims[4]; ++dim4) {
-            arr[dim0][dim1][dim2][dim3][dim4]
-                                = mxMalloc(sizeof(complex double) * dims[5]);
-          }
-        }
-      }
-    }
+  for(dim = 0; dim < 6; ++dim) {
+    (*A).dims[dim] = dims[dim];
   }
 
-  return arr;
+  (*A).arr = mxMalloc(sizeof(complex double) * NUMEL);
+
+  return A;
 }
 
 void free_Z3arr(az3_t *X)
 {
-  mwIndex dim, dim0, dim1;
-
-  mwSize dims[4];
-  for(dim = 0; dim < 4; ++dim) {
-    dims[dim] = (*X).dims[dim];
-  }
-
-  for(dim0 = (dims[0] - 1); dim0 > -1; --dim0) {
-    for(dim1 = (dims[1] - 1); dim1 > -1; --dim1) {
-      mxFree((*X).arr[dim0][dim1]);
-    }
-    mxFree((*X).arr[dim0]);
-  }
   mxFree((*X).arr);
+  mxFree(X);
 
   return;
 }
 
 void free_Z4arr(az4_t *X)
 {
-  mwIndex dim, dim0, dim1, dim2;
-
-  mwSize dims[4];
-  for(dim = 0; dim < 4; ++dim) {
-    dims[dim] = (*X).dims[dim];
-  }
-
-  for(dim0 = (dims[0] - 1); dim0 > -1; --dim0) {
-    for(dim1 = (dims[1] - 1); dim1 > -1; --dim1) {
-      for(dim2 = (dims[2] - 1); dim2 > -1; --dim2) {
-        mxFree((*X).arr[dim0][dim1][dim2]);
-      }
-      mxFree((*X).arr[dim0][dim1]);
-    }
-    mxFree((*X).arr[dim0]);
-  }
   mxFree((*X).arr);
+  mxFree(X);
 
   return;
 }
 
 void free_Z6arr(az6_t *X)
 {
-  mwIndex dim, dim0, dim1, dim2, dim3, dim4;
-
-  mwSize dims[6];
-  for(dim = 0; dim < 6; ++dim) {
-    dims[dim] = (*X).dims[dim];
-  }
-
-  for(dim0 = (dims[0] - 1); dim0 > -1; --dim0) {
-    for(dim1 = (dims[1] - 1); dim1 > -1; --dim1) {
-      for(dim2 = (dims[2] - 1); dim2 > -1; --dim2) {
-        for(dim3 = (dims[3] - 1); dim3 > -1; --dim3) {
-          for(dim4 = (dims[4] - 1); dim4 > -1; --dim4) {
-            mxFree((*X).arr[dim0][dim1][dim2][dim3][dim4]);
-          }
-          mxFree((*X).arr[dim0][dim1][dim2][dim3]);
-        }
-        mxFree((*X).arr[dim0][dim1][dim2]);
-      }
-      mxFree((*X).arr[dim0][dim1]);
-    }
-    mxFree((*X).arr[dim0]);
-  }
   mxFree((*X).arr);
+  mxFree(X);
 
   return;
 }
@@ -163,24 +103,47 @@ void free_Z6arr(az6_t *X)
 az3_t *MxToZ3Array(const mxArray *M)
 {
   const mwSize *MXDIMS = mxGetDimensions(M);
-  const double *MXDATAr = mxGetPr(M);
-  const double *MXDATAi = mxGetPi(M);
-  mwIndex dim, dim0, dim1, dim2, lindex;
+  const mwSize NDIMS = mxGetNumberOfDimensions(M);
+  const bool isComplex = mxIsComplex(M);
+  double *MXDATAr, *MXDATAi;
+  az3_t *A;
+  mwIndex lindex, dim;
+  mwSize dims[3];
+
+  if(isComplex) {
+    MXDATAr = mxGetPr(M);
+    MXDATAi = mxGetPi(M);
+  } else {
+    MXDATAr = mxGetPr(M);
+  }
+
+  /* pad any missing dimensions with 1 */
+  if(NDIMS < 3) {
+    for(dim = 0; dim < NDIMS; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+    for(dim = NDIMS; dim < 3; ++dim) {
+      dims[dim] = 1;
+    }
+  }
+  else {
+    for(dim = 0; dim < 3; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+  }
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2];
 
   /* initialise return struct */
-  az3_t *A = mxMalloc(sizeof(az3_t));
-  for(dim = 0; dim < 3; ++dim) {
-    (*A).dims[dim] = MXDIMS[dim];
-  }
-  (*A).arr = init_Z3arr(MXDIMS);
+  A = init_Z3arr(dims);
 
-  lindex = 0;
-  for(dim2 = 0; dim2 < (*A).dims[2]; ++dim2) {
-    for(dim1 = 0; dim1 < (*A).dims[1]; ++dim1) {
-      for(dim0 = 0; dim0 < (*A).dims[0]; ++dim0) {
-        (*A).arr[dim0][dim1][dim2] = MXDATAr[lindex] + I * MXDATAi[lindex];
-        ++lindex;
-      }
+  /* fill from mxArray, converting to complex double */
+  if(isComplex) {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * MXDATAi[lindex];
+    }
+  } else {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * 0.0;
     }
   }
 
@@ -190,27 +153,45 @@ az3_t *MxToZ3Array(const mxArray *M)
 az4_t *MxToZ4Array(const mxArray *M)
 {
   const mwSize *MXDIMS = mxGetDimensions(M);
-  const double *MXDATAr = mxGetPr(M);
-  const double *MXDATAi = mxGetPi(M);
-  mwIndex dim, dim0, dim1, dim2, dim3, lindex;
+  const mwSize NDIMS = mxGetNumberOfDimensions(M);
+  const bool isComplex = mxIsComplex(M);
+  double *MXDATAr, *MXDATAi;
+  az4_t *A;
+  mwIndex lindex, dim;
+  mwSize dims[4];
+
+  if(isComplex) {
+    MXDATAr = mxGetPr(M);
+    MXDATAi = mxGetPi(M);
+  } else {
+    MXDATAr = mxGetPr(M);
+  }
+
+  /* pad any missing dimensions with 1 */
+  if(NDIMS < 4) {
+    for(dim = 0; dim < NDIMS; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+    for(dim = NDIMS; dim < 4; ++dim) {
+      dims[dim] = 1;
+    }
+  } else {
+    for(dim = 0; dim < 4; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+  }
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2] * dims[3];
 
   /* initialise return struct */
-  az4_t *A = mxMalloc(sizeof(az4_t));
-  for(dim = 0; dim < 4; ++dim) {
-    (*A).dims[dim] = MXDIMS[dim];
-  }
-  (*A).arr = init_Z4arr(MXDIMS);
-
-  lindex = 0;
-  for(dim3 = 0; dim3 < (*A).dims[3]; ++dim3) {
-    for(dim2 = 0; dim2 < (*A).dims[2]; ++dim2) {
-      for(dim1 = 0; dim1 < (*A).dims[1]; ++dim1) {
-        for(dim0 = 0; dim0 < (*A).dims[0]; ++dim0) {
-          (*A).arr[dim0][dim1][dim2][dim3]
-                                        = MXDATAr[lindex] + I * MXDATAi[lindex];
-          ++lindex;
-        }
-      }
+  A = init_Z4arr(dims);
+  /* fill from mxArray, converting to complex double */
+  if(isComplex) {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * MXDATAi[lindex];
+    }
+  } else {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * 0.0;
     }
   }
 
@@ -220,31 +201,47 @@ az4_t *MxToZ4Array(const mxArray *M)
 az6_t *MxToZ6Array(const mxArray *M)
 {
   const mwSize *MXDIMS = mxGetDimensions(M);
-  const double *MXDATAr = mxGetPr(M);
-  const double *MXDATAi = mxGetPi(M);
-  mwIndex dim, dim0, dim1, dim2, dim3, dim4, dim5, lindex;
+  const mwSize NDIMS = mxGetNumberOfDimensions(M);
+  const bool isComplex = mxIsComplex(M);
+  double *MXDATAr, *MXDATAi;
+  az6_t *A;
+  mwIndex lindex, dim;
+  mwSize dims[6];
+
+  if(isComplex) {
+    MXDATAr = mxGetPr(M);
+    MXDATAi = mxGetPi(M);
+  } else {
+    MXDATAr = mxGetPr(M);
+  }
+
+  /* pad any missing dimensions with 1 */
+  if(NDIMS < 6) {
+    for(dim = 0; dim < NDIMS; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+    for(dim = NDIMS; dim < 6; ++dim) {
+      dims[dim] = 1;
+    }
+  } else {
+    for(dim = 0; dim < 6; ++dim) {
+      dims[dim] = MXDIMS[dim];
+    }
+  }
+  const mwSize NUMEL = dims[0] * dims[1] * dims[2]
+                        * dims[3] * dims[4] * dims[5];
 
   /* initialise return struct */
-  az6_t *A = mxMalloc(sizeof(az6_t));
-  for(dim = 0; dim < 6; ++dim) {
-    (*A).dims[dim] = MXDIMS[dim];
-  }
-  (*A).arr = init_Z6arr(MXDIMS);
+  A = init_Z6arr(dims);
 
-  lindex = 0;
-  for(dim5 = 0; dim5 < (*A).dims[5]; ++dim5) {
-    for(dim4 = 0; dim4 < (*A).dims[4]; ++dim4) {
-      for(dim3 = 0; dim3 < (*A).dims[3]; ++dim3) {
-        for(dim2 = 0; dim2 < (*A).dims[2]; ++dim2) {
-          for(dim1 = 0; dim1 < (*A).dims[1]; ++dim1) {
-            for(dim0 = 0; dim0 < (*A).dims[0]; ++dim0) {
-              (*A).arr[dim0][dim1][dim2][dim3][dim4][dim5]
-                                        = MXDATAr[lindex] + I * MXDATAi[lindex];
-              ++lindex;
-            }
-          }
-        }
-      }
+  /* fill from mxArray, converting to complex double */
+  if(isComplex) {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * MXDATAi[lindex];
+    }
+  } else {
+    for(lindex = 0; lindex < NUMEL; ++lindex) {
+      (*A).arr[lindex] = (complex double) MXDATAr[lindex] + I * 0.0;
     }
   }
 
@@ -256,20 +253,14 @@ mxArray *Z3ArrayToMx(az3_t *A)
   mxArray *M = mxCreateUninitNumericArray(3, (*A).dims, mxDOUBLE_CLASS,
                                           mxCOMPLEX);
   const mwSize NUMEL = (*A).dims[0] * (*A).dims[1] * (*A).dims[2];
-  mwIndex dim0, dim1, dim2, lindex;
+  mwIndex lindex;
 
   double *mxVecR = mxMalloc(sizeof(double) * NUMEL);
   double *mxVecI = mxMalloc(sizeof(double) * NUMEL);
 
-  lindex = 0;
-  for(dim2 = 0; dim2 < (*A).dims[2]; ++dim2) {
-    for(dim1 = 0; dim1 < (*A).dims[1]; ++dim1) {
-      for(dim0 = 0; dim0 < (*A).dims[0]; ++dim0) {
-        mxVecR[lindex] = creal((*A).arr[dim0][dim1][dim2]);
-        mxVecI[lindex] = cimag((*A).arr[dim0][dim1][dim2]);
-        ++lindex;
-      }
-    }
+  for(lindex = 0; lindex < NUMEL; ++lindex) {
+    mxVecR[lindex] = creal((*A).arr[lindex]);
+    mxVecI[lindex] = cimag((*A).arr[lindex]);
   }
 
   mxSetPr(M, mxVecR);
@@ -278,69 +269,99 @@ mxArray *Z3ArrayToMx(az3_t *A)
   return M;
 }
 
+mwIndex a3dex(const mwIndex dim0, const mwIndex dim1, const mwIndex dim2,
+              const mwSize dims[3])
+{
+  mwIndex lindex = dim2 * dims[1] * dims[0] + dim1 * dims[0] + dim0;
+
+  return lindex;
+}
+
+mwIndex a4dex(const mwIndex dim0, const mwIndex dim1, const mwIndex dim2,
+              const mwIndex dim3, const mwSize dims[4])
+{
+  mwIndex lindex = dim3 * dims[2] * dims[1] * dims[0] + dim2 * dims[1] * dims[0]
+                   + dim1 * dims[0] + dim0;
+
+  return lindex;
+}
+
+mwIndex a6dex(const mwIndex dim0, const mwIndex dim1, const mwIndex dim2,
+              const mwIndex dim3, const mwIndex dim4, const mwIndex dim5,
+              const mwSize dims[6])
+{
+  mwIndex lindex = dim5 * dims[4] * dims[3] * dims[2] * dims[1] * dims[0]
+                   + dim4 * dims[3] * dims[2] * dims[1] * dims[0]
+                   + dim3 * dims[2] * dims[1] * dims[0]
+                   + dim2 * dims[1] * dims[0] + dim1 * dims[0] + dim0;
+
+  return lindex;
+}
+
 az3_t *GrowLeft(az4_t *siteTensor, az6_t *mpo, az3_t *leftBlock,
                 const mwSize ROW_SIZE, const mwSize COL_SIZE,
                 const mwSize HILBY, const mwSize OP_ROW, const mwSize OP_COL)
 {
   mwIndex row, col, conjRow, conjCol, bra, ket;
-  mwIndex conjBra, conjKet, opRow, opCol, dim;
+  mwIndex conjBra, conjKet, opRow, opCol;
+  mwIndex sitedex, conjdex, mpodex, leftdex, updex;
+  mwSize numel, updims[3];
   complex double FA, WFA, AWFA;
+  az3_t *updateBlock;
 
   /* create conjugate site tensor */
-  az4_t *conjTensor = mxMalloc(sizeof(az4_t));
-  for(dim = 0; dim < 4; ++dim) {
-    (*conjTensor).dims[dim] = (*siteTensor).dims[dim];
-  }
-  (*conjTensor).arr = init_Z4arr((*conjTensor).dims);
-  for(row = 0; row < ROW_SIZE; ++row) {
-    for(col = 0; col < COL_SIZE; ++col) {
-      for(bra = 0; bra < HILBY; ++bra) {
-        for(ket = 0; ket < HILBY; ++ket) {
-          (*conjTensor).arr[row][col][bra][ket]
-            = conj((*siteTensor).arr[row][col][bra][ket]);
-        }
-      }
-    }
+  az4_t *conjTensor = init_Z4arr((*siteTensor).dims);
+  numel = (*siteTensor).dims[0] * (*siteTensor).dims[1]
+          * (*siteTensor).dims[2] * (*siteTensor).dims[3];
+  for(conjdex = 0; conjdex < numel; ++conjdex) {
+    (*conjTensor).arr[conjdex] = conj((*siteTensor).arr[conjdex]);
   }
 
   /* allocate return */
-  az3_t *updateBlock = mxMalloc(sizeof(az3_t));
-  (*updateBlock).dims[0] = COL_SIZE;
-  (*updateBlock).dims[1] = OP_COL;
-  (*updateBlock).dims[2] = COL_SIZE;
-  (*updateBlock).arr = init_Z3arr((*updateBlock).dims);
+  updims[0] = COL_SIZE;
+  updims[1] = OP_COL;
+  updims[2] = COL_SIZE;
+  updateBlock = init_Z3arr(updims);
 
   /* loop the loop! */
-  for(conjRow = 0; conjRow < COL_SIZE; ++conjRow) {
+  updex = 0;
+  for(col = 0; col < COL_SIZE; ++col) {
     for(opCol = 0; opCol < OP_COL; ++opCol) {
-      for(col = 0; col < COL_SIZE; ++col) {
+      for(conjRow = 0; conjRow < COL_SIZE; ++conjRow) {
         AWFA = 0.0 + I * 0.0;
         for(conjCol = 0; conjCol < ROW_SIZE; ++conjCol) {
           for(conjBra = 0; conjBra < HILBY; ++conjBra) {
             for(conjKet = 0; conjKet < HILBY; ++conjKet) {
+              conjdex = a4dex(conjCol, conjRow, conjBra, conjKet, (*conjTensor).dims);
+
               WFA = 0.0 + I * 0.0;
               for(bra = 0; bra < HILBY; ++bra) {
                 for(ket = 0; ket < HILBY; ++ket) {
                   for(opRow = 0; opRow < OP_ROW; ++opRow) {
+                    mpodex = a6dex(conjBra, conjKet, bra, ket, opRow, opCol, (*mpo).dims);
+
                     FA = 0.0 + I * 0.0;
                     for(row = 0; row < ROW_SIZE; ++row) {
-                      FA += (*leftBlock).arr[conjCol][opRow][row] * (*siteTensor).arr[row][col][bra][ket];
+                      leftdex = a3dex(conjCol, opRow, row, (*leftBlock).dims);
+                      sitedex = a4dex(row, col, bra, ket, (*siteTensor).dims);
+
+                      FA += (*leftBlock).arr[leftdex] * (*siteTensor).arr[sitedex];
                     }
-                    WFA += (*mpo).arr[conjBra][conjKet][bra][ket][opRow][opCol] * FA;
+                    WFA += (*mpo).arr[mpodex] * FA;
                   }
                 }
               }
-              AWFA += (*conjTensor).arr[conjCol][conjRow][conjBra][conjKet] * WFA;
+              AWFA += (*conjTensor).arr[conjdex] * WFA;
             }
           }
         }
-        (*updateBlock).arr[conjRow][opCol][col] = AWFA;
+        (*updateBlock).arr[updex] = AWFA;
+        ++updex;
       }
     }
   }
 
   free_Z4arr(conjTensor);
-  mxFree(conjTensor);
 
   return updateBlock;
 }
@@ -371,10 +392,6 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   free_Z4arr(siteTensor);
   free_Z6arr(mpo);
   free_Z3arr(leftBlock);
-  mxFree(updateBlock);
-  mxFree(siteTensor);
-  mxFree(mpo);
-  mxFree(leftBlock);
 
   return;
 }
