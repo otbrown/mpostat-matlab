d07e879d14d2af8b0ab688003417c86cd066056e
Oliver Thomson Brown <otbrown91@gmail.com>
2017-03-15 16:12:33 +0000

    EigenSolverTest.m: new version of primme produces different (but still useful) results, so test updated accordingly, tests for now non-existent error throws removed

--- testing/coreTests/EigenSolverTest.m
+++ testing/coreTests/EigenSolverTest.m
@@ -7,34 +7,26 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
     properties
         absTol = 1E-14;
         effL = sparse([2, -1, -1, 1; -1i, 1i, -1, 1; 0, 1, 0, 2; 0, 1i, 0, 2i]);
+        initVec = ones(4,1) + 1i * ones(4,1);
     end
 
     methods (Test)
         function testThrowBadHermiticity(tc)
-            tc.fatalAssertError(@()EigenSolver(tc.effL, true, 1E-9), ...
-            'EigenSolver:badHermiticity');
-        end
-
-        function testThrowBadHermiticityThreshold(tc)
-            tc.fatalAssertError(@()EigenSolver(tc.effL, true,  ...
-            rand(3, 1)), 'EigenSolver:badHermiticityThreshold');
-        end
-
-        function testThrowBadInitVec(tc)
-            tc.fatalAssertError(@()EigenSolver(tc.effL, false, 1), ...
-            'EigenSolver:badInitVec');
+            tc.fatalAssertError(@()EigenSolver(tc.effL, true, ...
+                        tc.initVec, 1E-9), 'EigenSolver:badHermiticity');
         end
 
         function testHermitian(tc)
             HERMITIAN = true;
             H = ctranspose(tc.effL) * tc.effL;
-            [vec, val] = EigenSolver(H, HERMITIAN);
+            [vec, val] = EigenSolver(H, HERMITIAN, tc.initVec);
 
             actualVal = 0;
-            actualVec = [ 0.302322027807821 + 0.345148819050045i;
-                          0.626057451236754 + 0.366562214671157i;
-                         -0.334442121239489 + 0.140454316093354i;
-                         -0.313028725618377 - 0.183281107335578i];
+
+            actualVec = [-0.450257651351328 - 0.088285690737856i;
+                         -0.719529322395919 + 0.092700289568880i;
+                          0.178778680891222 - 0.315621815829032i;
+                          0.359764661197959 - 0.046350144784440i];
 
             tc.assertLessThan(abs(actualVal - val), tc.absTol);
             tc.assertLessThan(abs(actualVec - vec), tc.absTol);
@@ -42,7 +34,7 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture('../../dev', 
 
         function testNonHermitian(tc)
             HERMITIAN = false;
-            [vec, val] = EigenSolver(tc.effL, HERMITIAN);
+            [vec, val] = EigenSolver(tc.effL, HERMITIAN, tc.initVec);
 
             actualVal = 1.920620363439616 + 0.618180731783746i;
             actualVec = [0.909915536697958;
